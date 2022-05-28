41d19a7401929bf1864cbed4f8b34e6e45f4d16d
sejinniemma <jsj0471@naver.com>
2022-05-28 09:05:23 +0900

    using axios with Youtub class to make it more readable

--- src/service/youtube.js
+++ src/service/youtube.js
@@ -1,25 +1,34 @@
+import axios from "axios";
+
 class Youtube {
     constructor (key) {
-        this.key = key;
-        this.getrequestOptions = {
-            method: 'GET',
-            redirect: 'follow'
-          };
+     this.youtube = axios.create({
+       baseURL: 'https://youtube.googleapis.com/youtube/v3',
+       params:{key: key},
+     })
     }
 
     async mostPopular(){
-       const response = await fetch(`https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=${this.key}`,
-        this.getrequestOptions
-      );
-      const result_1 = await response.json();
-      return result_1.items;
+      const response = await this.youtube.get('videos',{
+        params:{
+          part: 'snippet',
+          chart:'mostPopular',
+          maxResults:25,
+        }
+      })
+      return response.data.items;
     }
 
     async search(query){
-     const response = await fetch(`https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=${query}&maxResults=25&type=video&key=${this.key}`,
-        this.getrequestOptions);
-      const result_1 = await response.json();
-      return result_1.items.map(item => ({ ...item, id: item.id.videoId }));
+     const response = await this.youtube.get('search',{
+       params:{
+         part:'snippet',
+         q:query,
+         type:'video',
+         maxResults:25,
+       }
+     })
+     return response.data.items.map(item => ({...item, id: item.id.videoId}))
     }
 }
 
